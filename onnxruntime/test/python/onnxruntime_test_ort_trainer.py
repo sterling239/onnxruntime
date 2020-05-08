f0a6dad5c2b70e24222ab315c7e880cb948f9247
BowenBao <bowbao@microsoft.com>
2020-05-08 15:50:27 -0700

    Remove 'model_.' prefix for onnx model initializers in training

--- onnxruntime/test/python/onnxruntime_test_ort_trainer.py
+++ onnxruntime/test/python/onnxruntime_test_ort_trainer.py
@@ -258,9 +258,10 @@ class MNISTWrapper():
         model_desc = MNISTWrapper.mnist_model_description()
         return model, model_desc
 
-    def get_trainer(self, model, model_desc, device):
-        return ORTTrainer(model, MNISTWrapper.my_loss, model_desc, "SGDOptimizer", None, IODescription('Learning_Rate', [1, ],
-                                torch.float32), device, _opset_version=12)
+    def get_trainer(self, model, model_desc, device, frozen_weights=[]):
+        return ORTTrainer(model, MNISTWrapper.my_loss, model_desc, "SGDOptimizer", None,
+                          IODescription('Learning_Rate', [1, ], torch.float32), device,
+                          _opset_version=12, frozen_weights=frozen_weights)
 
 class TestOrtTrainer(unittest.TestCase):
     def testMNISTTrainingAndTesting(self):
@@ -405,6 +406,51 @@ class TestOrtTrainer(unittest.TestCase):
 
         trainer.save_as_onnx(onnx_file_name)
         assert os.path.exists(onnx_file_name)
+
+    def testMNISTInitializerNames(self):
+        torch.manual_seed(1)
+        device = torch.device("cuda")
+
+        mnist = MNISTWrapper()
+        train_loader, test_loader = mnist.get_loaders()
+        model, model_desc = mnist.get_model()
+
+        for model_device in [torch.device('cpu'), torch.device('cuda')]:
+            model.to(model_device)
+            trainer = mnist.get_trainer(model, model_desc, device)
+            learningRate = 0.02
+            epoch = 0
+
+            data, target = next(iter(train_loader))
+            data, target = data.to(device), target.to(device)
+            data = data.reshape(data.shape[0], -1)
+
+            loss, _ = trainer.train_step(data, target, torch.tensor([learningRate]))
+
+            assert set([n.name for n in trainer.onnx_model_.graph.initializer]) \
+                == set([n for n, t in model.named_parameters()])
+
+    def testMNISTFrozenWeight(self):
+        torch.manual_seed(1)
+        device = torch.device("cuda")
+
+        mnist = MNISTWrapper()
+        train_loader, test_loader = mnist.get_loaders()
+        model, model_desc = mnist.get_model()
+
+        for model_device in [torch.device('cpu'), torch.device('cuda')]:
+            model.to(model_device)
+            trainer = mnist.get_trainer(model, model_desc, device, frozen_weights=['fc1.weight'])
+
+            learningRate = 0.02
+            epoch = 0
+
+            data, target = next(iter(train_loader))
+            data, target = data.to(device), target.to(device)
+            data = data.reshape(data.shape[0], -1)
+
+            loss, _ = trainer.train_step(data, target, torch.tensor([learningRate]))
+            assert 'fc1.weight' not in trainer.state_dict() and 'fc2.weight' in trainer.state_dict()
 
     def testBertTrainingBasic(self):
         expected_losses = [
